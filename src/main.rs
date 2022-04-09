507259fd4a1be71f93eed83825d860ebc98c0ab2
woodsjc <woodsjc@gmail.com>
2022-04-09 16:41:21 -0500

    add sqlx version

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+mod sqlx_db;
 mod tokio_db;
 
-use tokio_postgres::Error;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //tokio version
     tokio_db::tokio_db().await?;
+    sqlx_db::sqlx_db().await?;
 
     Ok(())
 }
